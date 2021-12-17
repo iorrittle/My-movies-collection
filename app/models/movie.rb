2b7082221f6495e2adb3c9bb45b37425d7419795
iori matumoto <machulive.822@gmail.com>
2021-12-17 14:48:36 +0900

    movieモデルのバリデーション

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,17 @@
 class Movie < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :genre
 
+  validates :title ,presence: true
+  validates :director ,presence: true
+  validates :starring, presence: true
+  validates :genre_id, presence: true
+  validates :public_year_id, presence: true
+  validates :favorite_scene_1, presence: true
 
+  validates :genre_id, numericality:{ other_than: 1, message: "can't be blank"}
+  validates :public_year_id, numericality:{ other_than: 1, message: "can't be blank"}
 
 
 end
