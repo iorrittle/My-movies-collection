20056345f7d57b8d3988381c4f8006f339c653a0
iori matumoto <machulive.822@gmail.com>
2021-12-20 12:40:25 +0900

    index画面にバックグラウンドを実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,12 +9,17 @@ class MoviesController < ApplicationController
   end
   
   def create
-    
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to root_path(@movie)
+    else
+      render :new
+    end
   end  
 
   private
 
-  def message_params
+  def movie_params
     params.require(:movie).permit(:image, :title, :director, :starring, :genre_id, :public_year_id, :favorite_scene_1, :favorite_scene_2, :favorite_scene_3, :film_music, :artist_name, :memorandum).merge(user_id: current_user.id)
   end
 
