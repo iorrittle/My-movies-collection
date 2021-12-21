27d62620166b12c8b8a0f522050cf8690eb7cf43
iori matumoto <machulive.822@gmail.com>
2021-12-21 22:35:29 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @nickname = current_user.nickname
+    @movies = current_user.movies
+    @user = @user = User.find(params[:id])
   end
 end
