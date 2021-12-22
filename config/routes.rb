b6d0aa22cec03b16584ae8a6be293c9b4e731e25
iori matumoto <machulive.822@gmail.com>
2021-12-23 02:31:05 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "movies#index"
   resources :movies 
+  resources :rooms do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
-  resources :rooms, only: :new
 end
