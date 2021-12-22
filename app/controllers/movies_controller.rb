2af6b8d6a018274c9bcd4dc7951266f35701a31c
iori matumoto <machulive.822@gmail.com>
2021-12-22 09:53:44 +0900

    編集機能の実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,20 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end  
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie.id)
+    else 
+      render :edit
+    end
+  end  
+  
   private
 
   def movie_params
