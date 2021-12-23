2123b90e189e4e51710e7ac8abd98935c8f10125
iori matumoto <machulive.822@gmail.com>
2021-12-23 17:30:20 +0900

    画面の調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,8 +20,9 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, room_id: params[:room_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, room_id: params[:room_id])
   end
-end
 
 end
+
+
