876cc5f9dd39a4622bafc64aca703787ee6baf36
iori matumoto <machulive.822@gmail.com>
2022-02-21 22:27:29 +0900

    チャットルーム調整（長文に適応できるよう）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,21 @@ class CommentsController < ApplicationController
   
   
   def create
-    
     @room = Room.find(params[:room_id])
     @comments = @room.comments.order(created_at: :desc)
     @comment = @room.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save(created_at: :desc)
-      
       render :index
     else
       render :index
     end
-    
-  
   end
+  
   def destroy
     @room = Room.find(params[:room_id])
     @comment = Comment.find(params[:id])
     @comments = @room.comments.order(created_at: :desc)
-
     @comment.destroy
       render :index
 
